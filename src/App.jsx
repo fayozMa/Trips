1afa4dc07bb09d5ed389990a0ea43fdce62b510c
Fayyozbek <usmonovbahriddin108@gmail.com>
2024-06-02 17:29:49 +0500

    cemplete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TripsList from './components/TripsList'
+function App() {
+  
+  return (
+    <div className="App">
+      <TripsList/>
+    </div>
+  )
+}
+
+export default App
